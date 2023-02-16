d02b9d5c106514e168ef1a6b4241bdf6780f9e2a
ahmed <eng.ahmed.esmail19@gmail.com>
2023-02-16 12:16:14 -0800

    ASD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main prints "Programming is like building a multilingual puzzle, followed by a new line 
+ * return 0
+ */
 int main(){
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
