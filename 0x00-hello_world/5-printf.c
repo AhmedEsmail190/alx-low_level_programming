ef4d9cbabbfcb11270a19be80b24ba6f94b067d8
ahmed <eng.ahmed.esmail19@gmail.com>
2023-02-16 12:43:22 -0800

    ASD

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * main - prints "with proper grammar, but the outcome is 
  * a piece of art," followed newline
  * Return: ALways 0.
- *
  */
 
 int main(void)
+
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
