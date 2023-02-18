0bb916516b7b4a99ed023126ea04584ca43cf0f8
ahmed <eng.ahmed.esmail19@gmail.com>
2023-02-17 17:31:11 -0800

    ASD

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Always (Success).
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for ( i = 0; i < 16; i++)
+	{
+		if (i<10)
+			putchar('0'+i);
+	}
+	for ( i = 'a'; i < 'g'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	
+	return (0);
+}
