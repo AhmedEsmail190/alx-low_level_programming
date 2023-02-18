02d5bf59f0be2c579c068c8732267c7cb621e588
ahmed <eng.ahmed.esmail19@gmail.com>
2023-02-17 17:04:09 -0800

    ASD

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets except 'q' and 'e'
+ * Return: Always 0.;
+ */
+
+int main(void)
+{
+	
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
